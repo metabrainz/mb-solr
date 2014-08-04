221f5fac4cb405d51e93800dcd1afde85d222e55
Wieland Hoffmann <themineo@gmail.com>
2014-08-04 14:50:41 +0200

    Add Object as the element type of various lists

--- src/main/java/org/musicbrainz/search/solrwriter/MBXMLWriter.java
+++ src/main/java/org/musicbrainz/search/solrwriter/MBXMLWriter.java
@@ -57,7 +57,7 @@ public class MBXMLWriter implements QueryResponseWriter {
 	 * set the list on the {@link Metadata} object.
 	 */
 	class MetadataListWrapper {
-		private List objList = null;
+		private List<Object> objList = null;
 		private Object MMDList = null;
 		private Metadata metadata = null;
 		private Method setCountMethod = null;
@@ -87,7 +87,7 @@ public class MBXMLWriter implements QueryResponseWriter {
 			}
 		}
 
-		public List getLiveList() {
+		public List<Object> getLiveList() {
 			return objList;
 		}
 
@@ -183,7 +183,7 @@ public class MBXMLWriter implements QueryResponseWriter {
 		metadatalistwrapper.setCountAndOffset(con.docs.matches(),
 				con.docs.offset());
 
-		List xmlList = metadatalistwrapper.getLiveList();
+		List<Object> xmlList = metadatalistwrapper.getLiveList();
 
 		float maxScore = con.docs.maxScore();
 		DocIterator iter = con.docs.iterator();
