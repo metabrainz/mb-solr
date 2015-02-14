997c35639593408df0c779384d9fb6b82519a807
Wieland Hoffmann <themineo@gmail.com>
2015-02-14 01:14:22 +0100

    move testing code into a separate method

--- src/test/java/org/musicbrainz/search/solrwriter/MBXMLWriterTest.java
+++ src/test/java/org/musicbrainz/search/solrwriter/MBXMLWriterTest.java
@@ -3,31 +3,37 @@ package org.musicbrainz.search.solrwriter;
 import org.apache.solr.SolrTestCaseJ4;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.net.URI;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MBXMLWriterTest extends SolrTestCaseJ4{
-	@BeforeClass
-	public static void beforeClass() throws Exception {
-		initCore("solrconfig.xml", "schema.xml", "mbsssss", "artist");
-		String xmlfilepath = MBXMLWriterTest.class.getResource("artist.xml").getFile();
+	private void performCoreTest(String corename, ArrayList<String> documentValues) throws Exception {
+		initCore("solrconfig.xml", "schema.xml", "mbsssss", corename);
+		String xmlfilepath = MBXMLWriterTest.class.getResource(corename + ".xml").getFile();
 		byte[] content = Files.readAllBytes(Paths.get(xmlfilepath));
 		String xml = new String(content);
-		assertU(adoc("mbid", "9b58672a-e68e-4972-956e-a8985a165a1f", "artist", "Howard Shore", "sortname", "Shore, " +
-				"Howard", "_store", xml));
+		documentValues.add(0, xml);
+		documentValues.add(0, "_store");
+		assertU(adoc((documentValues.toArray(new String[documentValues.size()]))));
 		assertU(commit());
+
+		xmlfilepath = MBXMLWriterTest.class.getResource(corename + "-list.xml").getFile();
+		content = Files.readAllBytes(Paths.get(xmlfilepath));
+		xml = new String(content);
+		assertEquals(xml, h.query(req("q", "*:*", "fl", "score", "wt", "mbxml")));
+		deleteCore();
+	}
+	@Test
+	public void testArtist() throws Exception {
+		ArrayList<String> doc = new ArrayList<String>(Arrays.asList(new String[]{
+				"mbid",	"9b58672a-e68e-4972-956e-a8985a165a1f",
+				"artist", "Howard Shore",
+				"sortname", "Shore, Howard"}));
+		performCoreTest("artist", doc);
 	}
 
 	@Test
-	public void testMeep() throws Exception {
-		String xmlfilepath = MBXMLWriterTest.class.getResource("artist-list.xml").getFile();
-		byte[] content = Files.readAllBytes(Paths.get(xmlfilepath));
-		String xml = new String(content);
-		assertEquals(xml, h.query(req("q", "Howard Shore", "fl", "score", "wt", "mbxml")));
 	}
 }
