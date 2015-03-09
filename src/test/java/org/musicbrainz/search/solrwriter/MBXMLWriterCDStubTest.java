ea036aa5cb2911a960caa7a0dcf66d6631b90832
Wieland Hoffmann <themineo@gmail.com>
2015-03-09 14:38:29 +0100

    Add a test for cdstubs

---
+++ src/test/java/org/musicbrainz/search/solrwriter/MBXMLWriterCDStubTest.java
@@ -0,0 +1,14 @@
+package org.musicbrainz.search.solrwriter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MBXMLWriterCDStubTest extends MBXMLWriterTest{
+	static {
+		corename = "cdstub";
+		doc = new ArrayList<>(Arrays.asList(new String[]{
+				"id", "1",
+				"discid", "zsXyqGWvw0zF024A_saTokxIMzo-",
+				"title", "Doo-lang Doo-lang"}));
+	}
+}
