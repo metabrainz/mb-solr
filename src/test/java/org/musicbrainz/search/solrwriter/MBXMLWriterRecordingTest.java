2c3afd659aadb739049aeeaa906b45aa169f10ae
Wieland Hoffmann <themineo@gmail.com>
2015-03-10 15:40:35 +0100

    Add a test for recordings

---
+++ src/test/java/org/musicbrainz/search/solrwriter/MBXMLWriterRecordingTest.java
@@ -0,0 +1,20 @@
+package org.musicbrainz.search.solrwriter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MBXMLWriterRecordingTest extends MBXMLWriterTest {
+	static {
+		corename = "recording";
+		doc = new ArrayList<>(Arrays.asList(new String[]{
+				"mbid", uuid,
+				"recording", "Roots and Beginnings",
+				"name", "Roots and Beginnings",
+				"arid", "9b58672a-e68e-4972-956e-a8985a165a1f",
+				"artist", "Howard Shore",
+				"artistname", "Howard Shore",
+				"creditname", "Howard Shore",
+				"video", "false"
+		}));
+	}
+}
