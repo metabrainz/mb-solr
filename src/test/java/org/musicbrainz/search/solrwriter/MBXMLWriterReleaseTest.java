59cbd72f59ee1b8bc8c46c1928c966cd1b21ff68
Wieland Hoffmann <themineo@gmail.com>
2015-03-10 16:04:50 +0100

    Add a test for releases

---
+++ src/test/java/org/musicbrainz/search/solrwriter/MBXMLWriterReleaseTest.java
@@ -0,0 +1,15 @@
+package org.musicbrainz.search.solrwriter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MBXMLWriterReleaseTest extends MBXMLWriterTest{
+	static {
+		corename = "release";
+		doc = new ArrayList<>(Arrays.asList(new String[]{
+				"mbid", uuid,
+				"artist", "Chiptunes = WIN \\m|♥|m/",
+				"artistname", "Chiptunes = WIN \\m|♥|m/",
+				"release", "Chiptunes = WIN \\m|♥|m/"}));
+	}
+}
