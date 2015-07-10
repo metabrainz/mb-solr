2eb998acab833cffeed4a314535de370c1815965
jeffweeksio <jeff@jeffweeks.io>
2015-07-10 00:18:08 -0400

    copyright

--- src/main/java/org/musicbrainz/search/solrwriter/MBXMLWriter.java
+++ src/main/java/org/musicbrainz/search/solrwriter/MBXMLWriter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, Wieland Hoffmann
+ * Copyright (c) 2014-2015, Wieland Hoffmann and Jeff Weeks
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
