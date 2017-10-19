33d518d3bbdd551c57c9c2442400bdd500f0d263
Sambhav Kothari <sambhavs.email@gmail.com>
2017-10-20 00:59:42 +0530

    Add docstrings for MBError class

--- src/main/java/org/musicbrainz/search/solrwriter/MBError.java
+++ src/main/java/org/musicbrainz/search/solrwriter/MBError.java
@@ -7,11 +7,20 @@ import javax.xml.bind.annotation.XmlType;
 @XmlRootElement(name = "error")
 @XmlType(propOrder = {"code", "message"})
 public class MBError {
+    /**
+     * Custom XML bindings for JAXB to produce meaningful
+     * errors compatible with MB SolrQueryWriters in XML and
+     * JSON formats. It wraps SOLR Exceptions to a class that can
+     * be marshalled.
+     */
 
     private int code;
     private String message;
 
     @XmlElement(name = "code")
+    /**
+     * HTTP Error code returned by SOLR.
+     */
     public int getCode() {
         return code;
     }
@@ -21,6 +30,9 @@ public class MBError {
     }
 
     @XmlElement(name = "message")
+    /**
+     * Error string extracted from the SOLR exception
+     */
     public String getMessage() {
         return message;
     }
