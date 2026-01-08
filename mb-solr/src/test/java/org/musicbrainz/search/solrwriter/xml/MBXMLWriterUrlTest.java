9306ca2e52d7a61652c9b1ed87140c1bbaee95ef
Michael Wiencek <mwtuea@gmail.com>
2026-01-07 18:33:01 -0600

    Add URL tests

---
+++ mb-solr/src/test/java/org/musicbrainz/search/solrwriter/xml/MBXMLWriterUrlTest.java
@@ -0,0 +1,10 @@
+package org.musicbrainz.search.solrwriter.xml;
+
+import org.musicbrainz.search.solrwriter.AbstractMBWriterUrlTest;
+import org.musicbrainz.search.solrwriter.MBXMLWriterTestInterface;
+
+public class MBXMLWriterUrlTest extends AbstractMBWriterUrlTest implements MBXMLWriterTestInterface {
+	static {
+		corename = "url";
+	}
+}
